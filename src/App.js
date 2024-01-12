81f7b7bb5a77272af97238a25ec4c3d879cb0388
Siddhesh Khairnar <khairnarsiddhesh4057@gmail.com>
2024-01-13 00:00:53 +0530

    Learn useLocation Hook in React

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NoteState from "./context/notes/NoteState";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/about" element={<About />} />
-        </Routes>
-      </BrowserRouter>
+      <NoteState>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container my-3">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About/>}/>
+          </Routes>
+          </div>
+        </BrowserRouter>
+      </NoteState>
     </>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      
+      <div className="container my-3">
+      <h2>Add a Note</h2>  
+      </div> 
     </div>
   )
 }
