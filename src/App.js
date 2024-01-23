5a3922adb4766a2bdedd4183b28ba668b9071b96
Siddhesh Khairnar <khairnarsiddhesh4057@gmail.com>
2024-01-24 01:36:09 +0530

    SignUp and Login Component Added.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NoteState from "./context/notes/NoteState";
 import Alert from "./components/Alert"
 import { useState } from "react";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
   const [alert, setAlert] = useState(null);
@@ -28,6 +30,8 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route exact path="/about" element={<About/>}/>
+            <Route exact path="/login" element={<Login />}/>
+            <Route exact path="/signup"element={<Signup />}/>
           </Routes>
           </div>
         </BrowserRouter>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,24 +2,23 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props) => {
-  const host = "https://inotebook-backend-beta.vercel.app"
+  const host = "http://localhost:5000"
   const notesInitial = []
   const [notes, setNotes] = useState(notesInitial)
 
-  // Get all Notes
-  const getNotes = async () => {
-    // API Call 
-    const response = await fetch(`${host}/api/notes/fetchallnotes`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjU5ZThjNjUyNjk3YjhiOGQwMTgyNGY5In0sImlhdCI6MTcwNDk0NTEwN30.zv0NCZn3IfbH9LoXCnQxXMZWPLZFeEDvL4HsaTUKqY8"
-      }
-    });
-    const json = await response.json()
-    console.log(json)
-    setNotes(json)
-  }
+// Get all Notes
+const getNotes = async () => {
+  // API Call 
+  const response = await fetch(`${host}/api/notes/fetchallnotes`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjU5ZThjNjUyNjk3YjhiOGQwMTgyNGY5In0sImlhdCI6MTcwNDk0NTEwN30.zv0NCZn3IfbH9LoXCnQxXMZWPLZFeEDvL4HsaTUKqY8"
+    }
+  });
+  const json = await response.json();
+  setNotes(json);
+}
 
   //add a note
   const addNote = async (title, description, tag) => {
@@ -31,16 +30,7 @@ const NoteState = (props) => {
       },
       body: JSON.stringify({ title, description, tag })
     });
-    console.log("Adding a new note")
-    const note = {
-      "_id": "",
-      "user": "659e8c652697b8b8d01824f9",
-      "title": title,
-      "description": description,
-      "tag": tag,
-      "date": "2024-01-19T16:33:05.930+00:00",
-      "__v": 0
-    };
+    const note = await response.json();
     setNotes(notes.concat(note))
   }
   //delete a note
@@ -61,23 +51,28 @@ const NoteState = (props) => {
   // edit a note 
   const editNote = async (id, title, description, tag) => {
     const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
-      method: 'POST',
+      method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
         "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjU5ZThjNjUyNjk3YjhiOGQwMTgyNGY5In0sImlhdCI6MTcwNDk0NTEwN30.zv0NCZn3IfbH9LoXCnQxXMZWPLZFeEDvL4HsaTUKqY8"
       },
       body: JSON.stringify({ title, description, tag })
     });
+    
     const json = response.json();
+    console.log(json)
+    let newNotes = JSON.parse(JSON.stringify(notes))
     // Logic to edit in client
-    for (let index = 0; index < notes.length; index++) {
-      const element = notes[index];
+    for (let index = 0; index < newNotes.length; index++) {
+      const element = newNotes[index];
       if (element._id === id) {
         element.title = title;
         element.description = description;
         element.tag = tag;
+        break;
       }
     }
+    setNotes(newNotes);
   }
   return (
     <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote, getNotes}}>
